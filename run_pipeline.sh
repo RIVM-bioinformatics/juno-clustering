b9bf440a1520099cfb36e50909f9125d4290c97a
boasvdp <boasvdp@gmail.com>
2024-06-21 17:01:41 +0200

    feat: add run-wide presets

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,44 +4,74 @@ set -euo pipefail
 
 #----------------------------------------------#
 # User parameters
-if [ ! -z "${1}" ] || [ ! -z "${2}" ] #|| [ ! -z "${irods_input_projectID}" ]
+if [ ! -z "${1}" ] || [ ! -z "${2}" ] || [ ! -z "${irods_input_projectID}" ]
 then
-   input_dir="${1}"
-   output_dir="${2}"
-#    PROJECT_NAME="${irods_input_projectID}"
+    input_dir="${1}"
+    output_dir="${2}"
+    PROJECT_NAME="${irods_input_projectID}"
 else
+    echo "This shell script is only used to run the pipeline in the RIVM iRODS environment."
     echo "One of the parameters is missing, make sure there is an input directory, output directory and project name(param 1, 2 or irods_input_projectID)."
     exit 1
 fi
 
 if [ ! -d "${input_dir}" ] || [ ! -d "${output_dir}" ]
 then
-  echo "The input directory $input_dir, output directory $output_dir or fastq dir ${input_dir}/clean_fastq does not exist"
-  exit 1
-else
-  input_fastq="${input_dir}/clean_fastq"
+    echo "The input directory $input_dir or the output directory $output_dir does not exist"
+    exit 1
 fi
 
+# check if there is an exclusion file, if so change the parameter
+if [ ! -z "${irods_input_sequencing__run_id}" ] && [ -f "/data/BioGrid/NGSlab/sample_sheets/${irods_input_sequencing__run_id}.exclude" ]
+then
+  EXCLUSION_FILE_COMMAND="-ex /data/BioGrid/NGSlab/sample_sheets/${irods_input_sequencing__run_id}.exclude"
+else
+  EXCLUSION_FILE_COMMAND=""
+fi
+
+
 #----------------------------------------------#
-# Create/update necessary environments
-PATH_MAMBA_YAML="envs/mamba.yaml"
-PATH_MASTER_YAML="envs/template_master.yaml"
-MAMBA_NAME=$(head -n 1 ${PATH_MAMBA_YAML} | cut -f2 -d ' ')
-MASTER_NAME=$(head -n 1 ${PATH_MASTER_YAML} | cut -f2 -d ' ')
+## make sure conda works
 
-echo -e "\nUpdating necessary environments to run the pipeline..."
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/mnt/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/mnt/miniconda/etc/profile.d/conda.sh" ]; then
+        . "/mnt/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/mnt/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<export -f conda
+export -f __conda_activate
+export -f __conda_reactivate
+export -f __conda_hashr
 
-# Removing strict mode because it sometimes breaks the code for 
-# activating an environment and for testing whether some variables
-# are set or not
-set +euo pipefail 
 
-conda env update -f "${PATH_MAMBA_YAML}"
-source activate "${MAMBA_NAME}"
+#----------------------------------------------#
+## Run collfinder to get previous clustering run
 
-mamba env update -f "${PATH_MASTER_YAML}"
+mamba env create -f envs/collfinder.yaml --name collfinder_env
+conda activate collfinder_env
 
-source activate "${MASTER_NAME}"
+# Run collfinder.py in subshell
+PREVIOUS_RUN = $( python collfinder.py
+    -i "$input_dir"
+    -m projectID
+    -x "sys::pipeline::gitrepo=https://github.com/RIVM-bioinformatics/Juno_clustering"
+    -x "sys::data::state=valid"
+    -r import_timestamp )
+
+conda deactivate
+
+#----------------------------------------------#
+# Install pipeline conda env
+mamba env create -f envs/juno_clustering.yaml --name pipeline_env
+conda activate pipeline_env
 
 #----------------------------------------------#
 # Run the pipeline
@@ -54,9 +84,27 @@ else
     QUEUE="bio"
 fi
 
+case $PROJECT_NAME in
+  myco)
+    TYPE="mycobacterium_tuberculosis"
+    ;;
+  salm)
+    TYPE="salmonella"
+    ;;
+  *)
+    TYPE="unknown"
+    ;;
+esac
+
 set -euo pipefail
 
-python template.py --queue "${QUEUE}" -i "${input_dir}" -o "${output_dir}"
+python juno_clustering.py \
+    --queue "${QUEUE}" \
+    -i "${input_dir}" \
+    -o "${output_dir}" \
+    --clustering-preset "${TYPE}" \
+    --previous-clustering "${PREVIOUS_RUN}" \
+    "${EXCLUSION_FILE_COMMAND}"
 
 result=$?
 
